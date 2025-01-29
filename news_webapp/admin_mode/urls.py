77886190b95ca6aad1da55f1df43fb3d1c25298a
kimiakeivan <velizalester@gmail.com>
2025-01-29 18:21:29 +0330

    delete admin_mode app

--- news_webapp/admin_mode/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    # path('update/', views.fetch_and_store_news, name='fetch_and_store_news'),
-    path("update/", views.fetch_and_store_news, name="fetch_and_store_news"),
-    path("addnews/", views.addnews, name="addnews"),
-]
